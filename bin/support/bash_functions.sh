d7177e29e782262358721d10753510fa4c184a51
Matt Larson <coslars@gmail.com>
2018-10-02 13:19:33 -0600

    Logging

--- bin/support/bash_functions.sh
+++ bin/support/bash_functions.sh
@@ -23,6 +23,8 @@ heroku_buildpack_ruby_install_ruby()
   local buildpack_dir=$2
   heroku_buildpack_ruby_dir="$buildpack_dir/vendor/ruby/$STACK"
 
+  echo heroku_buildpack_ruby_dir
+
   # The -d flag checks to see if a file exists and is a directory.
   # This directory may be non-empty if a previous compile has
   # already placed a Ruby executable here. Also
